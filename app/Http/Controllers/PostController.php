1ca4786bf5aa84919ab2edfa65122634ce5cf4c7
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-13 12:38:29 +0900

    laravel9 コントローラー書き換えた

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    // 投稿作成
+    public function create(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+        return response();
+    }
+    // 全件取得
+    public function index()
+    {
+        return response()->json(Post::all());
+    }
+    // 投稿表示
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+    // 投稿編集
+    public function update(Request $request, Post $post) // Using Route Model Binding
+    {
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+        return response()->json($post);
+    }
+    // 投稿削除
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(Post::all());
+    }
 }
