037251ce256190622f83d999f02ebcec2a4bbe0a
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-13 11:38:28 +0900

    ルーティングの設定

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->group(function () {
+    // 投稿作成
+    Route::post('/posts/create', [PostController::class, 'create']);
+    // 投稿一覧表示
+    Route::get('/posts', [PostController::class, 'index']);
+    // 投稿表示
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    // 投稿編集
+    Route::patch('/posts/update/{id}', [PostController::class, 'update']);
+    // 投稿削除
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
 });
