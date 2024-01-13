6b923935a4372b056d316318092833f84b306253
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-13 11:36:09 +0900

    Postモデルクラス記載

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    // fillableに指定したプロパティは入力可能になる
+    //  $fillableプロパティは、一括割り当てを許可するすべての属性をリストする配列
+    protected $fillable = [
+        'title',
+        'content',
+    ];
 }
